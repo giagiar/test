39e9fb938b8d94da48d84dfef5133cf6ef3fa758
giagiar <giagiar.x@gmail.com>
2021-11-15 11:55:15 +0800

    Update i18n.js

--- jquery.i18n/i18n.js
+++ jquery.i18n/i18n.js
@@ -11,10 +11,10 @@ jQuery(document).ready(function() {
   });
 
   $.i18n().load({
-    'en': '{
+    'en': {
       'welcome': 'Welcome!',
       'messages-hello':'hihi'
-    }',
+    },
     'zh':'languages/zh.json'
   });
 
