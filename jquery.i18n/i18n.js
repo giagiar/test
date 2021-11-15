bf6dbfeafc22bd785df372bc424f556e995fb5a6
giagiar <giagiar.x@gmail.com>
2021-11-15 10:34:26 +0800

    test

--- jquery.i18n/i18n.js
+++ jquery.i18n/i18n.js
@@ -1,11 +1,11 @@
 function updateText() {
     $('body').i18n();
-    $('#messages').text($.i18n('message_from', 'Ann', 2, 'female'));
     
 }
 
 
 jQuery( document ).ready( function ( ) {
+    
     $('.lang-switch').click(function(e) {
     e.preventDefault();
     $.i18n().locale = $(this).data('locale');
@@ -13,11 +13,11 @@ jQuery( document ).ready( function ( ) {
     });
     
 	$.i18n().load( {
-	en: {
+	'en': {
 		'message-hello': 'Hello World',
         'welcome': 'Welcome-T',
 		'message-goodbye': 'Good bye'	},
-	zh: './languages/zh.json', // Messages for Mandarin
+	'zh': './languages/zh.json', // Messages for Mandarin
 
     } );
     
