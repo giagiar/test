d95cfcfc4485d83114bab49c6eba6904bb9bc102
giagiar <giagiar.x@gmail.com>
2021-11-15 10:21:42 +0800

    test

--- jquery.i18n/i18n.js
+++ jquery.i18n/i18n.js
@@ -1,15 +1,26 @@
 function updateText() {
-    $.i18n().load( {
+    $('body').i18n();
+    $('#messages').text($.i18n('message_from', 'Ann', 2, 'female'));
+    
+}
+
+
+jQuery( document ).ready( function ( ) {
+    $('.lang-switch').click(function(e) {
+    e.preventDefault();
+    $.i18n().locale = $(this).data('locale');
+    updateText();
+    });
+    
+	$.i18n().load( {
 	en: {
 		'message-hello': 'Hello World',
+        'welcome': 'Welcome-T',
 		'message-goodbye': 'Good bye'	},
 	zh: './languages/zh.json', // Messages for Mandarin
 
     } );
-}
-
-
-$( document ).ready( function ( $ ) {
-	'use strict';
-	updateText();
+    
+    updateText();
+    
 } );
