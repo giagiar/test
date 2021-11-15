6865e82516f5ad672f088d502138d12901257843
giagiar <giagiar.x@gmail.com>
2021-11-15 10:38:43 +0800

    test

--- jquery.i18n/i18n.js
+++ jquery.i18n/i18n.js
@@ -1,26 +1,25 @@
-function updateText() {
+jQuery(document).ready(function() {
+  var update_texts = function() {
     $('body').i18n();
-    
-}
+    $('#messages').text($.i18n('message_from', 'Ann', 2, 'female'));
+  };
 
-
-jQuery( document ).ready( function ( ) {
-    
-    $('.lang-switch').click(function(e) {
+  $('.lang-switch').click(function(e) {
     e.preventDefault();
     $.i18n().locale = $(this).data('locale');
-    updateText();
-    });
-    
-	$.i18n().load( {
-	'en': {
-		'message-hello': 'Hello World',
-        'welcome': 'Welcome-T',
-		'message-goodbye': 'Good bye'	},
-	'zh': './languages/zh.json', // Messages for Mandarin
+    update_texts();
+  });
 
-    } );
-    
-    updateText();
-    
-} );
+  $.i18n().load({
+    'en': {
+      'welcome': 'Welcome!',
+      'message_from': '$1 has send you $2 {{plural:$2|message|messages}}. {{gender:$3|He|She}} is waiting for your response!'
+    },
+    'zh': {
+      'welcome': '歡迎',
+      'message_from': '$1 {{gender:$3|отправил|отправила}} вам $2 {{plural:$2|сообщение|сообщений|сообщения}}. {{gender:$3|Он|Она}} ждёт ответа!'
+    }
+  });
+
+  update_texts();
+});
